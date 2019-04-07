aa0666fd588fa3b01595503d7ac435bb40f5ea96
Unix&Me <unixnme@gmail.com>
2019-04-07 14:29:59 +0900

    update client module interface

--- client/__main__.py
+++ client/__main__.py
@@ -1,9 +1,15 @@
+import argparse
 import sys
 from client.client import Client
 
 def main():
-    msg = sys.argv[1]
-    client = Client()
-    client.send(msg)
+    parser = argparse.ArgumentParser('Telegram client')
+    parser.add_argument('--host', type=str, default='localhost')
+    parser.add_argument('--port', type=int, default=9876)
+    parser.add_argument('msg', type=str, help='msg to send')
+    args = parser.parse_args()
+
+    client = Client(args.host, args.port)
+    client.send(args.msg)
 
 main()
